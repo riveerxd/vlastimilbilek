346102cb0ff34e98e18e0090a3f7e9e33124bc98
lukas.hrehor <lukas.hrehor@gmail.com>
2025-03-05 19:25:21 +0100

    updated password

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import { compare } from 'bcryptjs';
 // In production, these would be environment variables
 const USERNAME = 'admin';
 // Replace this with your newly generated hash
-const PASSWORD_HASH = '$2b$10$MSx3dmsHiU7/o2ocxCHwE.HEestv8D0INMEuXbx9VdUSNspEW1qs6';
+const PASSWORD_HASH = '$2b$10$jPq5HdSi5LMv5662qLf.deu3u1fikmtN9FZTSRVdPGIVYMKj3Cxiy';
 
 export async function authenticate(username: string, password: string) {
   if (username !== USERNAME) {
